b2872f7f790fc72c47e4e16e1265e0a228608529
MarkJeronimus <mark.jeronimus@gmail.com>
2022-06-12 15:05:18 +0200

    Working delta encoder

--- src/org/digitalmodular/qoiflow/TestMain.java
+++ src/org/digitalmodular/qoiflow/TestMain.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 
 import org.digitalmodular.qoiflow.instruction.QoiInstruction;
 import org.digitalmodular.qoiflow.instruction.QoiInstructionColorHistory;
+import org.digitalmodular.qoiflow.instruction.QoiInstructionDelta;
 import org.digitalmodular.qoiflow.instruction.QoiInstructionRGBA;
 import org.digitalmodular.qoiflow.instruction.QoiInstructionRunLength;
 
@@ -16,24 +17,26 @@ public class TestMain {
 	public static void main(String... args) {
 		QoiInstruction rle    = new QoiInstructionRunLength();
 		QoiInstruction hist   = new QoiInstructionColorHistory();
+		QoiInstruction diff8  = new QoiInstructionDelta(2, 2, 2, 2);
+		QoiInstruction diff16 = new QoiInstructionDelta(4, 4, 4, 4);
 		QoiInstruction rgba16 = new QoiInstructionRGBA(5, 6, 5, 0);
 		QoiInstruction rgba24 = new QoiInstructionRGBA(8, 8, 8, 0);
 		QoiInstruction rgba32 = new QoiInstructionRGBA(8, 8, 8, 8);
 
-		QoiFlowCodec enc = new QoiFlowCodec(Arrays.asList(rle, hist, rgba16, rgba24, rgba32));
+		QoiFlowCodec enc = new QoiFlowCodec(Arrays.asList(rle, hist, diff8, diff16, rgba16, rgba24, rgba32));
 		enc.setVariableLength(0, enc.getNumVariableCodes() >> 1);
 
 		ByteBuffer dst = ByteBuffer.allocate(100);
 
 		enc.reset();
 		enc.printCodeOffsets();
-		enc.encode(new QoiColor(1, 2, 3, 1), dst);
-		enc.encode(new QoiColor(1, 2, 3, 1), dst);
-		enc.encode(new QoiColor(1, 2, 3, 0), dst);
-		enc.encode(new QoiColor(4, 3, 2, 0), dst);
-		enc.encode(new QoiColor(4, 3, 2, 0), dst);
-		enc.encode(new QoiColor(4, 3, 2, 0), dst);
-		enc.encode(new QoiColor(1, 2, 3, 0), dst);
-		enc.encode(new QoiColor(1, 2, 3, 1), dst);
+		enc.encode(new QoiColor(0, 0, 0, 1), dst);
+		enc.encode(new QoiColor(0, 0, 1, 1), dst);
+		enc.encode(new QoiColor(0, 1, 1, 1), dst);
+		enc.encode(new QoiColor(1, 1, 1, 1), dst);
+		enc.encode(new QoiColor(1, 1, 1, 3), dst);
+		enc.encode(new QoiColor(1, 1, 3, 3), dst);
+		enc.encode(new QoiColor(1, 3, 3, 3), dst);
+		enc.encode(new QoiColor(3, 3, 3, 3), dst);
 	}
 }
---
+++ src/org/digitalmodular/qoiflow/instruction/QoiInstructionDelta.java
@@ -0,0 +1,67 @@
+package org.digitalmodular.qoiflow.instruction;
+
+import java.nio.ByteBuffer;
+
+import org.digitalmodular.qoiflow.QoiColor;
+import org.digitalmodular.qoiflow.QoiColorDelta;
+import org.digitalmodular.qoiflow.QoiPixelData;
+
+/**
+ * @author Mark Jeronimus
+ */
+// Created 2022-06-12
+public class QoiInstructionDelta extends QoiInstruction {
+	private final int numBytes;
+
+	public QoiInstructionDelta(int bitsR, int bitsG, int bitsB, int bitsA) {
+		super(bitsR, bitsG, bitsB, bitsA);
+
+		//noinspection OverridableMethodCallDuringObjectConstruction
+		numBytes = getMaxSize();
+	}
+
+	@Override
+	public int encode(QoiPixelData pixel, byte[] dst) {
+		QoiColorDelta delta = pixel.getDelta();
+
+		int r = (delta.deltaR() << shiftR) & maskR;
+		int g = (delta.deltaG() << shiftG) & maskG;
+		int b = (delta.deltaB() << shiftB) & maskB;
+		int a = delta.deltaA() & maskA;
+
+		if ((r << (32 - numBits)) >> (32 - bitsR) != delta.deltaR() ||
+		    (g << (32 - shiftR)) >> (32 - bitsG) != delta.deltaG() ||
+		    (b << (32 - shiftG)) >> (32 - bitsB) != delta.deltaB() ||
+		    (a << (32 - shiftB)) >> (32 - bitsA) != delta.deltaA()) {
+			return -1;
+		}
+
+		int rgba = r | g | b | a;
+
+		for (int i = 0; i < numBytes; i++) {
+			int shift = 8 * (numBytes - i - 1);
+			if (shift < 32)
+				dst[i] = (byte)(rgba >> shift);
+			else
+				dst[i] = 0;
+		}
+
+		dst[0] |= codeOffset;
+
+		return numBytes;
+	}
+
+	@Override
+	public void decode(ByteBuffer src, QoiColor color) {
+	}
+
+	@SuppressWarnings("StringConcatenationMissingWhitespace")
+	@Override
+	public String toString() {
+		if (bitsA > 0) {
+			return "DELTA" + bitsR + bitsR + bitsR + bitsA;
+		} else {
+			return "DELTA" + bitsR + bitsR + bitsR;
+		}
+	}
+}
