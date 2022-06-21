b07cc34ccc84fa091a658dc0d6b85f55fa134da7
MarkJeronimus <mark.jeronimus@gmail.com>
2022-06-21 22:49:30 +0200

    Batch test & Interactive test (verified Color History and RGBA)

---
+++ src/test/InteractiveTestMain.java
@@ -0,0 +1,169 @@
+package test;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.stream.IntStream;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+
+import org.digitalmodular.qoiflow.QOIEncoderStatistics;
+
+/**
+ * @author author
+ */
+// Created 2022-05-22
+// Changed 2022-06-18 Copied from FluidQOI
+public class InteractiveTestMain extends JPanel {
+	private static int fileIndex = 1;
+
+	private BufferedImage image1 = null;
+	private BufferedImage image2 = null;
+
+	public static void main(String... args) throws IOException {
+//		TestMain.collectImageFilesRecursively(TestMain.files, Paths.get("qoi_test_images"));
+		TestMain.collectImageFilesRecursively(TestMain.files, Paths.get("images-pixelart-tiles"));
+//		TestMain.collectImageFilesRecursively(TestMain.files, Paths.get("images-lance"));
+//		TestMain.collectImageFilesRecursively(TestMain.files, Paths.get("qoi_benchmark_suite"));
+		TestMain.files.sort(Comparator.comparing(Path::getFileName));
+
+		if (TestMain.files.isEmpty()) {
+			System.err.println("No files found");
+			System.exit(1);
+		}
+
+//		fileIndex = ThreadLocalRandom.current().nextInt(TestMain.files.size());
+		fileIndex = IntStream.range(0, TestMain.files.size())
+		                     .filter(i -> {
+			                     Path file = TestMain.files.get(i);
+			                     return file.getFileName().toString().startsWith("FA31389B");
+		                     })
+		                     .findFirst()
+		                     .orElse(0);
+
+		TestMain.codec.setStatistics(new QOIEncoderStatistics());
+
+		SwingUtilities.invokeLater(() -> {
+			JFrame f = new JFrame();
+			f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+			f.setContentPane(new InteractiveTestMain());
+
+			f.pack();
+			f.setLocationRelativeTo(null);
+			f.setVisible(true);
+		});
+	}
+
+	@SuppressWarnings("OverridableMethodCallDuringObjectConstruction")
+	public InteractiveTestMain() {
+		super(null);
+		setBackground(Color.WHITE);
+
+		fileIndex %= TestMain.files.size();
+
+		setPreferredSize(new Dimension(1024, 512));
+
+		// Yes I know it's bad practice to do work on the EDT, but it's a cheap test class anyway.
+
+		addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (e.getButton() == MouseEvent.BUTTON1) {
+					fileIndex = (fileIndex + 1) % TestMain.files.size();
+				} else if (e.getButton() == MouseEvent.BUTTON3) {
+					fileIndex =
+							(fileIndex + TestMain.files.size() - 1) % TestMain.files.size();
+				}
+
+				// Yes I know it's bad practice to do work on the EDT, but it's a cheap test class anyway.
+				convertImage();
+
+				repaint();
+			}
+		});
+
+		SwingUtilities.invokeLater(this::convertImage);
+	}
+
+	private void convertImage() {
+		Path file = TestMain.files.get(fileIndex);
+
+		Frame topLevelAncestor = (Frame)getTopLevelAncestor();
+		if (topLevelAncestor != null) {
+			topLevelAncestor.setTitle(fileIndex + ": " + file.getFileName());
+		}
+
+		BufferedImage[] images = TestMain.convertImage(file);
+		image1 = images[1];
+		image2 = images[2];
+
+		int mismatchPixel = TestMain.compareImages(images);
+		if (mismatchPixel >= 0) {
+			SwingUtilities.invokeLater(() -> JOptionPane.showMessageDialog(
+					this,
+					"Images differ at pixel " + mismatchPixel + " (" +
+					mismatchPixel % images[2].getWidth() + ", " + mismatchPixel / images[2].getWidth() + ')',
+					getClass().getSimpleName(),
+					JOptionPane.ERROR_MESSAGE));
+		}
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		if (image1 == null)
+			return;
+
+		drawTransparencyCheckerboard(g);
+
+		g.drawImage(image1,
+		            0, 0, getWidth() / 2, getHeight(),
+		            0, 0, image1.getWidth(), image1.getHeight(),
+		            null);
+		g.drawImage(image2,
+		            getWidth() / 2, 0, getWidth(), getHeight(),
+		            0, 0, image2.getWidth(), image2.getHeight(),
+		            null);
+
+		int scaleX = getWidth() / 2 / image1.getWidth();
+		int scaleY = getHeight() / image1.getHeight();
+		if (scaleX >= 16 && scaleY >= 16) {
+			g.setColor(Color.DARK_GRAY);
+			for (int u = 1; u < image1.getWidth() * 2; u++) {
+				int x = u * getWidth() / (image1.getWidth() * 2);
+				g.drawLine(x, 0, x, getHeight());
+			}
+			for (int v = 1; v < image1.getHeight(); v++) {
+				int y = v * getHeight() / image1.getHeight();
+				g.drawLine(0, y, getWidth(), y);
+			}
+			g.setColor(Color.BLACK);
+			g.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight());
+		}
+	}
+
+	private void drawTransparencyCheckerboard(Graphics g) {
+		int blockSize = 16;
+		for (int y = 0; y < getWidth(); y += blockSize) {
+			boolean b = (y / blockSize & 1) != 0;
+			for (int x = 0; x < getWidth(); x += blockSize) {
+				g.setColor(b ? Color.LIGHT_GRAY : Color.GRAY);
+				g.fillRect(x, y, blockSize, blockSize);
+				b = !b;
+			}
+		}
+	}
+}
---
+++ src/test/TestMain.java
@@ -0,0 +1,171 @@
+package test;
+
+import java.awt.AlphaComposite;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBuffer;
+import java.awt.image.DataBufferByte;
+import java.awt.image.DataBufferInt;
+import java.awt.image.DataBufferUShort;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+import javax.imageio.ImageIO;
+
+import org.digitalmodular.qoiflow.QoiFlowImageDecoder;
+import org.digitalmodular.qoiflow.QoiFlowImageEncoder;
+import org.digitalmodular.qoiflow.QoiFlowStreamCodec;
+import org.digitalmodular.qoiflow.instruction.QoiInstruction;
+import org.digitalmodular.qoiflow.instruction.QoiInstructionChroma;
+import org.digitalmodular.qoiflow.instruction.QoiInstructionColorHistory;
+import org.digitalmodular.qoiflow.instruction.QoiInstructionMaskRGBA;
+import org.digitalmodular.qoiflow.instruction.QoiInstructionRGBA;
+import org.digitalmodular.qoiflow.instruction.QoiInstructionRunLength;
+import org.digitalmodular.util.HexUtilities;
+
+/**
+ * @author Mark Jeronimus
+ */
+// Created 2022-05-22
+// Changed 2022-06-18 Copied from FluidQOI
+public class TestMain {
+	private static final QoiInstruction rle      = new QoiInstructionRunLength();
+	private static final QoiInstruction hist     = new QoiInstructionColorHistory();
+	private static final QoiInstruction chroma6  = new QoiInstructionChroma(2, 2, 2, 0);
+	private static final QoiInstruction chroma12 = new QoiInstructionChroma(3, 3, 3, 3);
+	private static final QoiInstruction rgba12   = new QoiInstructionRGBA(4, 4, 4, 0);
+	private static final QoiInstruction maskRGB  = new QoiInstructionMaskRGBA(false);
+	private static final QoiInstruction maskRGBA = new QoiInstructionMaskRGBA(true);
+	private static final QoiInstruction rgba24   = new QoiInstructionRGBA(8, 8, 8, 0);
+	private static final QoiInstruction rgba32   = new QoiInstructionRGBA(8, 8, 8, 8);
+
+	static final List<Path> files = new ArrayList<>(20000);
+
+	static QoiFlowStreamCodec codec;
+
+	static {
+		codec = new QoiFlowStreamCodec(Arrays.asList(hist, rgba32));
+//		codec.setVariableLength(0, codec.getNumVariableCodes() >> 1);
+	}
+
+	public static void main(String... args) throws IOException {
+//		collectImageFilesRecursively(files, Paths.get("qoi_test_images"));
+		collectImageFilesRecursively(files, Paths.get("images-pixelart-tiles"));
+//		collectImageFilesRecursively(files, Paths.get("images-lance"));
+//		collectImageFilesRecursively(files, Paths.get("qoi_benchmark_suite"));
+		files.sort(Comparator.comparing(Path::getFileName));
+
+		for (Path file : files) {
+			BufferedImage[] images = convertImage(file);
+
+			int mismatchPixel = compareImages(images);
+			if (mismatchPixel >= 0) {
+				System.out.println(
+						file.getFileName() + ": Images differ at pixel " + mismatchPixel + " / " +
+						images[0].getWidth() * images[0].getHeight() + " (" +
+						mismatchPixel % images[2].getWidth() + ", " + mismatchPixel / images[2].getWidth() + ')');
+			}
+		}
+	}
+
+	static void collectImageFilesRecursively(Collection<Path> files, Path path) throws IOException {
+		Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
+			@Override
+			public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+				super.visitFile(file, attrs);
+
+				if (file.getFileName().toString().endsWith(".png"))
+					files.add(file);
+
+				return FileVisitResult.CONTINUE;
+			}
+		});
+	}
+
+	static BufferedImage[] convertImage(Path file) {
+		BufferedImage image;
+		try {
+			image = ImageIO.read(file.toFile());
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			System.exit(1);
+			//noinspection ReturnOfNull
+			return null;
+		}
+
+		try {
+			ByteBuffer qoiData = new QoiFlowImageEncoder(codec).encode(image);
+
+			if (codec.getStatistics() != null) {
+				System.out.println(HexUtilities.hexArrayToString(qoiData.array(), qoiData.position(), 4, 8, 12, -5));
+			}
+
+			qoiData.flip();
+			BufferedImage image2 = new QoiFlowImageDecoder(codec).decode(qoiData);
+
+			BufferedImage image1 = new BufferedImage(image2.getWidth(), image2.getHeight(), image2.getType());
+			Graphics2D    g      = image1.createGraphics();
+			try {
+				g.setComposite(AlphaComposite.Src);
+				g.drawImage(image, 0, 0, null);
+			} finally {
+				g.dispose();
+			}
+
+//		String filename = file.getFileName().toString();
+//		filename = filename.substring(0, filename.length() - 4) + ".qoi565";
+//		Files.write(file.getParent().resolve(filename), qoi565.array());
+
+			return new BufferedImage[]{image, image1, image2};
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			System.exit(1);
+			return new BufferedImage[0];
+		}
+	}
+
+	static int compareImages(BufferedImage[] images) {
+		DataBuffer dataBuffer1 = images[1].getRaster().getDataBuffer();
+		DataBuffer dataBuffer2 = images[2].getRaster().getDataBuffer();
+
+		if (dataBuffer2 instanceof DataBufferByte) {
+			byte[] pixels1 = ((DataBufferByte)dataBuffer1).getData();
+			byte[] pixels2 = ((DataBufferByte)dataBuffer2).getData();
+			for (int i = 0; i < pixels1.length; i++) {
+				if (pixels1[i] != pixels2[i]) {
+					return i / images[2].getColorModel().getNumComponents();
+				}
+			}
+		} else if (dataBuffer2 instanceof DataBufferUShort) {
+			short[] pixels1 = ((DataBufferUShort)dataBuffer1).getData();
+			short[] pixels2 = ((DataBufferUShort)dataBuffer2).getData();
+			for (int i = 0; i < pixels1.length; i++) {
+				if (pixels1[i] != pixels2[i]) {
+					return i;
+				}
+			}
+		} else if (dataBuffer2 instanceof DataBufferInt) {
+			int[] pixels1 = ((DataBufferInt)dataBuffer1).getData();
+			int[] pixels2 = ((DataBufferInt)dataBuffer2).getData();
+			for (int i = 0; i < pixels1.length; i++) {
+				if (pixels1[i] != pixels2[i]) {
+					return i;
+				}
+			}
+		} else {
+			throw new AssertionError("Unknown data buffer: " + dataBuffer2.getClass());
+		}
+
+		return -1;
+	}
+}
